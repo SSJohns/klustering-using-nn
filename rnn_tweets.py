3153e47674cffb0c0a46fcc4c2b6093a47cff2b3
SSJohns <sjohns25@nd.edu>
2017-03-19 19:34:10 -0400

    test script and data

--- rnn_tweets.py
+++ rnn_tweets.py
@@ -7,54 +7,30 @@ from keras.layers import LSTM
 from keras.callbacks import ModelCheckpoint
 from keras.utils import np_utils
 
-def gen(gen_model):
-    # load the network weights
-    filename = "weights-improvement-19-1.9435.hdf5"
-    gen_model.load_weights(filename)
-    gen_model.compile(loss='categorical_crossentropy', optimizer='adam')
 
-    # pick a random seed
-    start = numpy.random.randint(0, len(dataX)-1)
-    pattern = dataX[start]
-    print "Seed:"
-    print "\"", ''.join([int_to_char[value] for value in pattern]), "\""
-    # generate characters
-    for i in range(1000):
-        x = numpy.reshape(pattern, (1, len(pattern), 1))
-        x = x / float(n_vocab)
-        prediction = gen_model.predict(x, verbose=0)
-        index = numpy.argmax(prediction)
-        result = int_to_char[index]
-        seq_in = [int_to_char[value] for value in pattern]
-        sys.stdout.write(result)
-        pattern.append(index)
-        pattern = pattern[1:len(pattern)]
-    print "\nDone."
-
-def load():
-    # load asci`i text and covert to lowercase
-    filename = "wonderland.txt"
-    raw_text = open(filename).read()
-    raw_text = raw_text.lower()
-    # create mapping of unique chars to integers
-    chars = sorted(list(set(raw_text)))
-    char_to_int = dict((c, i) for i, c in enumerate(chars))
-    # summarize the loaded data
-    n_chars = len(raw_text)
-    n_vocab = len(chars)
-    print "Total Characters: ", n_chars
-    print "Total Vocab: ", n_vocab
-    # prepare the dataset of input to output pairs encoded as integers
-    seq_length = 100
-    dataX = []
-    dataY = []
-    for i in range(0, n_chars - seq_length, 1):
-        seq_in = raw_text[i:i + seq_length]
-        seq_out = raw_text[i + seq_length]
-        dataX.append([char_to_int[char] for char in seq_in])
-        dataY.append(char_to_int[seq_out])
-    n_patterns = len(dataX)
-    print "Total Patterns: ", n_patterns
+# load asci`i text and covert to lowercase
+filename = "wonderland.txt"
+raw_text = open(filename).read()
+raw_text = raw_text.lower()
+# create mapping of unique chars to integers
+chars = sorted(list(set(raw_text)))
+char_to_int = dict((c, i) for i, c in enumerate(chars))
+# summarize the loaded data
+n_chars = len(raw_text)
+n_vocab = len(chars)
+print "Total Characters: ", n_chars
+print "Total Vocab: ", n_vocab
+# prepare the dataset of input to output pairs encoded as integers
+dataX = []
+dataY = []
+seq_length = 100
+for i in range(0, n_chars - seq_length, 1):
+    seq_in = raw_text[i:i + seq_length]
+    seq_out = raw_text[i + seq_length]
+    dataX.append([char_to_int[char] for char in seq_in])
+    dataY.append(char_to_int[seq_out])
+n_patterns = len(dataX)
+print "Total Patterns: ", n_patterns
 # reshape X to be [samples, time steps, features]
 X = numpy.reshape(dataX, (n_patterns, seq_length, 1))
 # normalize
@@ -74,4 +50,26 @@ callbacks_list = [checkpoint]
 # fit the model
 model.fit(X, y, nb_epoch=20, batch_size=128, callbacks=callbacks_list)
 
-gen(model)
+
+# load the network weights
+filename = "weights-improvement-19-1.9435.hdf5"
+model.load_weights(filename)
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+
+# pick a random seed
+start = numpy.random.randint(0, len(dataX)-1)
+pattern = dataX[start]
+print "Seed:"
+print "\"", ''.join([int_to_char[value] for value in pattern]), "\""
+# generate characters
+for i in range(1000):
+    x = numpy.reshape(pattern, (1, len(pattern), 1))
+    x = x / float(n_vocab)
+    prediction = model.predict(x, verbose=0)
+    index = numpy.argmax(prediction)
+    result = int_to_char[index]
+    seq_in = [int_to_char[value] for value in pattern]
+    sys.stdout.write(result)
+    pattern.append(index)
+    pattern = pattern[1:len(pattern)]
+print "\nDone."
